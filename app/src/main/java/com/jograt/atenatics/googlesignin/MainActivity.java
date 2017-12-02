1d9fe53282dd9a69d0a37b4806e1f328b0b9761d
John Cuthpert Y. Magbanua <johnmagbanua1997@gmail.com>
2017-12-02 12:26:35 +0800

    new login UI

--- app/src/main/java/com/jograt/atenatics/googlesignin/MainActivity.java
+++ app/src/main/java/com/jograt/atenatics/googlesignin/MainActivity.java
@@ -35,8 +35,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("ReCash");
-
         mAuth = FirebaseAuth.getInstance();
 
         mGooglebtn = (SignInButton)findViewById(R.id.googleSignin);
